6cf14ad875f3379e7310eadb7210d736d94c86a6
大野真和 <oonomasakazu@oonomasakazunoMacBook-Pro.local>
2021-11-08 14:23:03 +0900

    added DB

--- ogijima/admin.py
+++ ogijima/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Work,Blog,Gallery,Notification,Profile,Art,Restaurant,Hotel,Cat
 
 # Register your models here.
+admin.site.register(Work)
+admin.site.register(Blog)
+admin.site.register(Gallery)
+admin.site.register(Notification)
+admin.site.register(Profile)
+admin.site.register(Art)
+admin.site.register(Restaurant)
+admin.site.register(Hotel)
+admin.site.register(Cat)
