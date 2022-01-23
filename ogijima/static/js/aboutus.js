bdc22e97eac274ae687e046f2fbd3c0b3da2760b
HirotoInomoto <hiroto_inomoto@demia.co.jp>
2022-01-23 16:42:50 +0900

    topaboutus更新

--- ogijima/static/js/aboutus.js
+++ ogijima/static/js/aboutus.js
@@ -6,43 +6,42 @@ var sec4Offset = document.getElementById('business').offsetTop;
 var sec5Offset = document.getElementById('source').offsetTop;
 var sec6Offset = document.getElementById('footer').offsetTop;
 
-window.addEventListener('scroll', () => {
-    var scrollY = window.pageYOffset;
-    if(scrollY > sec3Offset - screenHeight * 0.1){
-        document.getElementById('bgimage-1').style.opacity = 0;
-        document.getElementById('bgimage-2').style.opacity = 0;
-        document.getElementById('bgimage-3').style.opacity = 1;
-    }else if(scrollY > sec2Offset - screenHeight * 0.4){
-        document.getElementById('bgimage-1').style.opacity = 0;
-        document.getElementById('bgimage-2').style.opacity = 1;
-        document.getElementById('bgimage-3').style.opacity = 0;
-    }else{
-        document.getElementById('bgimage-1').style.opacity = 1;
-        document.getElementById('bgimage-2').style.opacity = 0;
-        document.getElementById('bgimage-3').style.opacity = 0;
-    }
-    // if(scrollY > sec4Offset - screenHeight * 0.4){
-    //     document.getElementById('bgimage-1').style.opacity = 0;
-    //     document.getElementById('bgimage-2').style.opacity = 0;
-    //     document.getElementById('bgimage-3').style.opacity = 0;
-    //     document.getElementById('bgimage-4').style.opacity = 1;
-    // }else if(scrollY > sec3Offset - screenHeight * 0.4){
-    //     document.getElementById('bgimage-1').style.opacity = 0;
-    //     document.getElementById('bgimage-2').style.opacity = 0;
-    //     document.getElementById('bgimage-3').style.opacity = 1;
-    //     document.getElementById('bgimage-4').style.opacity = 0;
-    // }else if(scrollY > sec2Offset - screenHeight * 0.4){
-    //     document.getElementById('bgimage-1').style.opacity = 0;
-    //     document.getElementById('bgimage-2').style.opacity = 1;
-    //     document.getElementById('bgimage-3').style.opacity = 0;
-    //     document.getElementById('bgimage-4').style.opacity = 0;
-    // }else{
-    //     document.getElementById('bgimage-1').style.opacity = 1;
-    //     document.getElementById('bgimage-2').style.opacity = 0;
-    //     document.getElementById('bgimage-3').style.opacity = 0;
-    //     document.getElementById('bgimage-4').style.opacity = 0;
-    // }
-})
+if(userAgent == "pc"){
+    window.addEventListener('scroll', () => {
+        var scrollY = window.pageYOffset;
+        if(scrollY > sec3Offset - screenHeight * 0.1){
+            document.getElementById('bgimage-1').style.opacity = 0;
+            document.getElementById('bgimage-2').style.opacity = 0;
+            document.getElementById('bgimage-3').style.opacity = 1;
+        }else if(scrollY > sec2Offset - screenHeight * 0.4){
+            document.getElementById('bgimage-1').style.opacity = 0;
+            document.getElementById('bgimage-2').style.opacity = 1;
+            document.getElementById('bgimage-3').style.opacity = 0;
+        }else{
+            document.getElementById('bgimage-1').style.opacity = 1;
+            document.getElementById('bgimage-2').style.opacity = 0;
+            document.getElementById('bgimage-3').style.opacity = 0;
+        }
+    })
+}else{
+    window.addEventListener('scroll', () => {
+        var scrollY = window.pageYOffset;
+        if(scrollY > sec3Offset - screenHeight * 0.1){
+            document.getElementById('bgimage-1').style.opacity = 0;
+            document.getElementById('bgimage-2').style.opacity = 0;
+            document.getElementById('bgimage-3').style.opacity = 1;
+        }else if(scrollY > sec2Offset - screenHeight * 0.5){
+            document.getElementById('bgimage-1').style.opacity = 0;
+            document.getElementById('bgimage-2').style.opacity = 1;
+            document.getElementById('bgimage-3').style.opacity = 0;
+        }else{
+            document.getElementById('bgimage-1').style.opacity = 1;
+            document.getElementById('bgimage-2').style.opacity = 0;
+            document.getElementById('bgimage-3').style.opacity = 0;
+        }
+    })
+   
+}
 
 if(userAgent == "pc"){
     document.getElementById('bgimage-1').src = "https://lh3.googleusercontent.com/pw/AM-JKLU6I9G5GIj73WKm90o95kxQR0LKorlDlHvcXtp44blZi1wBIbrN661i09sxu1kj7_pOTiNNIFW1kPVbY-ro9c31pMvCvp1pD_DOzhzu6utrvOl15CwFRK6HUdF1iL_f0V4R7uEK4-K33K_gHCzn4guz=s0";
